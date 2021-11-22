98e652fdc980354df0ee6ba124b59ced1eebc01d
unknown <juan.sueldo1989@gmail.com>
2021-11-22 17:58:10 -0300

    TP4 entrega

---
+++ TP4/TP4.sql
@@ -0,0 +1,8 @@
+/****** Script para el comando SelectTopNRows de SSMS  ******/
+SELECT TOP (1000) [idLegajo]
+      ,[nombre]
+      ,[apellido]
+      ,[genero]
+      ,[puesto]
+      ,[edad]
+  FROM [TP4].[dbo].[Programadores]
